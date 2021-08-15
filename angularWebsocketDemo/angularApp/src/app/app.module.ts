d9d2e1e8e30801b12df3ec44955ad87680cece2d
Nipun <shehanatuk@gmail.com>
2021-08-14 23:02:11 -0400

    added to environment.ts file

--- angularWebsocketDemo/angularApp/src/app/app.module.ts
+++ angularWebsocketDemo/angularApp/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { MessageComponent } from './message/message.component';
+import { MyChatComponent } from './my-chat/my-chat.component';
+import { OthersChatComponent } from './others-chat/others-chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    MessageComponent
+    MessageComponent,
+    MyChatComponent,
+    OthersChatComponent
   ],
   imports: [
     BrowserModule,
---
+++ angularWebsocketDemo/angularApp/src/app/others-chat/others-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-others-chat',
+  templateUrl: './others-chat.component.html',
+  styleUrls: ['./others-chat.component.css']
+})
+export class OthersChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
